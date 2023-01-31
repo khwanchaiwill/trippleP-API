55bbe2725bc3b3c9ab57e07cc1c16d50ba03f07d
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2023-01-31 17:15:35 -0500

    update ignore

--- Api/server.js
+++ Api/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
+const express = require('express')
 const helmet = require('helmet');
-const cors = require('cors')
-
+const cors = require('cors');
 const server = express();
 
-server.use(helmet());
+const UserRouter = require('../router/userRouter')
+const CategoryRouter = require('../router/Categories')
+
+
+
+server.use(helmet())
 server.use(express.json());
-server.use(cors());
+server.use(cors())
 server.use(logger)
 
+server.use("/api/users", UserRouter) 
+server.use("/api/category", CategoryRouter)
 server.get("/", (req, res) => {
-    res.status(200).json({Hello: " World, The api is working"})
+    res.send('Testing the api')
 })
+
 function logger (req, res, next){
     console.log(`${req.method} request the ${req.url}`, Date())
     next();
 }
 
 module.exports = server;
+
 
