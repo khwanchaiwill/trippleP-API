3db83f4d7e941f6a744f88470fc9ae6d1eea3227
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2023-01-31 17:29:04 -0500

    change data

--- data/seeds/admin.js
+++ data/seeds/admin.js
@@ -3,9 +3,16 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
+  const admin = [
+    {
+      name: "admin",// will get id 1
+      username: "admin",
+      password: "serafina0607",
+      email: "porpan00@hotmail.com"
+    },
+  ];
 
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-  ]);
+  return knex("admins")
+  .insert(admin)
+  .then(() => console.log("\n== Seed data for admins table added. ==\n"));
 };
