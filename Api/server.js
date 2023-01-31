9f7c4d06fdc7404049ab3d77bdf1d9d57dc2023a
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2023-01-31 18:51:18 -0500

    delete file

--- Api/server.js
+++ Api/server.js
@@ -3,18 +3,12 @@ const helmet = require('helmet');
 const cors = require('cors');
 const server = express();
 
-const UserRouter = require('../router/userRouter')
-const CategoryRouter = require('../router/Categories')
-
-
-
 server.use(helmet())
 server.use(express.json());
 server.use(cors())
 server.use(logger)
 
-server.use("/api/users", UserRouter) 
-server.use("/api/category", CategoryRouter)
+
 server.get("/", (req, res) => {
     res.send('Testing the api')
 })
