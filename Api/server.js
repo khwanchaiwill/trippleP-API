8726d584e760aabf8462282c8ba5c85e4aaad7e9
khwanchaiwill <64489050+khwanchaiwill@users.noreply.github.com>
2023-02-02 00:19:35 -0500

    change file

--- Api/server.js
+++ Api/server.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const helmet = require('helmet');
+const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet')
 const server = express();
 
-server.use(helmet())
+server.use(helmet());
 server.use(express.json());
-server.use(cors())
-server.use(logger)
-
+server.use(cors());
+server.use(logger);
 
 server.get("/", (req, res) => {
-    res.send('Testing the api')
-})
-
-function logger (req, res, next){
-    console.log(`${req.method} request the ${req.url}`, Date())
+    res.status(200).json({ Hello: "World, The api is now very working" });
+  });
+  function logger(req, res, next) {
+    console.log(`${req.method} request the ${req.url}`, Date());
     next();
-}
-
-module.exports = server;
-
-
+  }
+  
+  module.exports = server;
+  
